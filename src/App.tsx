cc022c94406d5d11e9f83877626328286aaf251b
Tabunov Egor <Vonubat@gmail.com>
2022-12-12 08:26:11 +0300

    feat: add Header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Background } from 'components/ui';
+import { Header } from 'layout';
 import React from 'react';
 
 export const App = (): JSX.Element => {
   return (
     <div className="App">
+      <Header />
       <Background />
     </div>
   );
---
+++ src/layout/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+export const Header = (): JSX.Element => {
+  return <header className={styles.header}>Notes Editor</header>;
+};
