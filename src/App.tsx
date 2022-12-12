22f441a0c797b97011f49beea10a759facafd496
Tabunov Egor <Vonubat@gmail.com>
2022-12-13 02:30:43 +0300

    refactor: optimize folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Background } from 'components/ui';
 import { ContextProvider } from 'context/Context';
 import { Header, Main } from 'layout';
+import { Background } from 'components';
 import React from 'react';
 
 export const App = (): JSX.Element => {
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { Textarea } from './Textarea';
+export { Background } from './Background';
